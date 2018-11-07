7f8691425d317415c714c53c18dc7d7b21ff7432
Carlos Eduardo Ardila Patiño <43663237+CarlosArdilaP@users.noreply.github.com>
2018-11-07 18:22:26 -0300

    actialicaodoasd
    pico pal que lee

---
+++ ProCableWW1/src/clasesproyecto/Sucursal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesproyecto;
+
+public class Sucursal {
+    //private ListaFactura facturasSucursales;   
+    private String nombreSucursal;
+    private String direccion Surcursal;
+    private String rutSucursal;
+    private 
+}
---
+++ ProCableWW1/src/clasesproyecto/listas/ListaFactura.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesproyecto.listas;
+import java.util.*;
+import clasesproyecto.*;
+
+public class ListaFactura {
+    private ArrayList<Factura> facturas;
+    
+    
+    public ListaFactura(){
+        this.facturas = new ArrayList<Factura>();
+    }
+    
+    
+}
