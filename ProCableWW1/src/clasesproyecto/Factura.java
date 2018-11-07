9afce9431c724ba8f00c7c9ef05da7e290868f24
LuisMaturanaC <luis.maturana.96@gmail.com>
2018-11-07 20:48:19 -0300

    Factura/ListaFactura

--- ProCableWW1/src/clasesproyecto/Factura.java
+++ ProCableWW1/src/clasesproyecto/Factura.java
@@ -5,10 +5,12 @@ package clasesproyecto;
 public class Factura {
     private Cliente clienteAbonante;
     private float montoPagado;
+    private int idFactura;
     
-    public Factura(Cliente clienteAbonante, float montoPagado){
+    public Factura(Cliente clienteAbonante, float montoPagado, int idFactura){
         this.clienteAbonante = clienteAbonante;
         this.montoPagado = montoPagado;
+        this.idFactura = idFactura;
     }
 
     public Cliente getClienteAbonante() {
@@ -18,6 +20,10 @@ public class Factura {
     public float getMontoPagado() {
         return montoPagado;
     }
+    
+    public int getIdFactura() {
+        return idFactura;
+    }
 
     public void setClienteAbonante(Cliente clienteAbonante) {
         this.clienteAbonante = clienteAbonante;
@@ -26,5 +32,8 @@ public class Factura {
     public void setMontoPagado(float montoPagado) {
         this.montoPagado = montoPagado;
     }
-    
+
+    public void setIdFactura(int idFactura) {
+        this.idFactura = idFactura;
+    }
 }
--- ProCableWW1/src/clasesproyecto/listas/ListaFactura.java
+++ ProCableWW1/src/clasesproyecto/listas/ListaFactura.java
@@ -15,4 +15,29 @@ public class ListaFactura {
         this.facturas = new ArrayList<Factura>();
     }
     
+    public boolean estaVacio() {
+        return facturas.isEmpty();
+    }
+    
+    public int tamano(){
+        return facturas.size();
+    }
+    
+    public Factura buscarFactura (int idFactura){
+        if(estaVacio()){
+            return null;
+        }
+        else{
+            for(int i = 0; i < tamano(); i++){
+                if(facturas.get(i).getIdFactura() == idFactura){
+                    return facturas.get(i);
+                }
+            }
+        }
+        return null;
+    }
+    
+    public ArrayList<Factura> buscarFacturasCliente (String rutCliente){
+        
+    }
 }
