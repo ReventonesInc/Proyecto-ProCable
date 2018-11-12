2617fb63af978ece693189ed63ba460dded42a2e
LuisMaturanaC <luis.maturana.96@gmail.com>
2018-11-11 22:02:01 -0300

    Comentado de ListaCliente y ListaFactura

--- ProCableWW1/src/clasesproyecto/listas/ListaCliente.java
+++ ProCableWW1/src/clasesproyecto/listas/ListaCliente.java
@@ -8,13 +8,13 @@ public class ListaCliente {
 	
     private ArrayList<Cliente> clientes;
 	
-    //-------------------------------------- Constructor ------------------------------------------------//
+//-------------------------------------- Constructor ------------------------------------------------//
     
     public ListaCliente() {
         this.clientes = new ArrayList<Cliente>();
     }	
 	
-    //---------------------------------------Setters y getters ------------------------------------------//
+//---------------------------------------Setters y getters ------------------------------------------//
 	
     public ArrayList<Cliente> getClientes() {	
         return clientes;
@@ -24,18 +24,18 @@ public class ListaCliente {
         this.clientes = clientes;
     }
     
-    //--------------------------------------- M�todos-----------------------------------------------------//
+//--------------------------------------- Metodos-----------------------------------------------------//
 	
-    public boolean estaVacio() {								//Reemplazo para el metodo isEmpty
+    public boolean estaVacio() {						//Reemplazo para el metodo isEmpty
         return clientes.isEmpty();
     }
 	
-    public int tamano() {										//Reemplazo para el metodo size
+    public int tamano() {							//Reemplazo para el metodo size
         return clientes.size();
     }
 	
-    public int indice(Cliente buscado) {						//Metodo para retornar el indice del array
-        if(estaVacio()) {										//Si la lista esta vacia retorna un valor fuera del vector
+    public int indice(Cliente buscado) {					//Metodo para retornar el indice del array
+        if(estaVacio()) {							//Si la lista esta vacia retorna un valor fuera del vector
             return -1;									
         }
         else {								
@@ -49,11 +49,11 @@ public class ListaCliente {
     }
     
     public int indice(String rut) {						//Metodo para retornar el indice del array
-        if(estaVacio()) {										//Si la lista esta vacia retorna un valor fuera del vector
+        if(estaVacio()) {							//Si la lista esta vacia retorna un valor fuera del vector
             return -1;									
         }
         else {								
-            for(int i = 0; i < tamano(); i++) {					//Sino se busca el cleinte y se retorna su posicion	
+            for(int i = 0; i < tamano(); i++) {					//Sino se busca el cliente y se retorna su posicion	
                 if(clientes.get(i).getRut().equals(rut)){
                     return i;
 		}
@@ -63,74 +63,74 @@ public class ListaCliente {
     }
         
     public Cliente buscarCliente(Cliente buscado) {				//Metodo para buscar un cliente en la lista
-        if(estaVacio()) {										//Si la lista esta vacia
-            return null;										//No se encuentra ningun cliente y retorna null
+        if(estaVacio()) {							//Si la lista esta vacia
+            return null;							//No se encuentra ningun cliente y retorna null
         }
-        else {													//Sino, la lista no esta vacia y hay que recorrerla
+        else {									//Sino, la lista no esta vacia y hay que recorrerla
             for(int i = 0; i < tamano(); i++) {					//Ciclo que recorrera la ArrayList
-                if(clientes.get(i).equals(buscado)) {			//Busca el cliente y si lo encuentra lo retorna
+                if(clientes.get(i).equals(buscado)) {                           //Busca el cliente y si lo encuentra lo retorna
                     return clientes.get(i);
             	}
             }
-            return null;										//En caso contrario, ya que no lo encuentra retorna null
+            return null;							//En caso contrario, ya que no lo encuentra retorna null
         }
     }
 	
-    public Cliente buscarCliente(String rutCliente) {			//Metodo para buscar un cliente en la lista usando su rut
-        if(estaVacio()) {										//Si la lista esta vacia
-            return null;										//No se encuentra ningun rut que coincida y retorna null
+    public Cliente buscarCliente(String rutCliente) {                           //Metodo para buscar un cliente en la lista usando su rut
+        if(estaVacio()) {							//Si la lista esta vacia
+            return null;							//No se encuentra ningun rut que coincida y retorna null
         }
-        else {													//Sino, la lista no esta vacia y hay que recorrela
+        else {									//Sino, la lista no esta vacia y hay que recorrela
             for(int i = 0; i < tamano(); i++) {
-                if(clientes.get(i).getRut().equals(rutCliente)) {	//Busca el rut en el ArrayList esperando una coincidencia
-                    return clientes.get(i);							//Si lo encuentra, retorna el cliente con dicho rut
+                if(clientes.get(i).getRut().equals(rutCliente)) {               //Busca el rut en el ArrayList esperando una coincidencia
+                    return clientes.get(i);					//Si lo encuentra, retorna el cliente con dicho rut
                 }
             }
-            return null;										//En caso contrario retorna null, ya que no encontro el rut
+            return null;							//En caso contrario retorna null, ya que no encontro el rut
         }
     }
     
     public boolean buscarClienteMenu(String rutCliente){
-        if(estaVacio()) {										//Si la lista esta vacia
-            return false;										//No se encuentra ningun rut que coincida y retorna null
+        if(estaVacio()) {							//Si la lista esta vacia
+            return false;							//No se encuentra ningun rut que coincida y retorna null
         }
-        else {													//Sino, la lista no esta vacia y hay que recorrela
+        else {									//Sino, la lista no esta vacia y hay que recorrela
             for(int i = 0; i < tamano(); i++) {
-                if(clientes.get(i).getRut().equals(rutCliente)) {	//Busca el rut en el ArrayList esperando una coincidencia
-                    return true;							//Si lo encuentra, retorna el cliente con dicho rut
+                if(clientes.get(i).getRut().equals(rutCliente)) {               //Busca el rut en el ArrayList esperando una coincidencia
+                    return true;						//Si lo encuentra, retorna el cliente con dicho rut
                 }
             }
-            return false;										//En caso contrario retorna null, ya que no encontro el rut
+            return false;							//En caso contrario retorna null, ya que no encontro el rut
         }
     }
     
     public boolean agregarCliente(Cliente nuevoCliente) {
-        if(estaVacio()) {										//Si la lista esta vacia
-            clientes.add(nuevoCliente);							//Agrega el cliente al final
+        if(estaVacio()) {							//Si la lista esta vacia
+            clientes.add(nuevoCliente);						//Agrega el cliente al final
             return true;
         }
-        else {													//Si no esta vacia
+        else {									//Si no esta vacia
             if(buscarCliente(nuevoCliente)!=null) {				//Busca el cliente
-                return false;									//Si lo encuentra retorna false
+                return false;							//Si lo encuentra retorna false
             }
-            else {												//Si lo encuentra
+            else {								//Si lo encuentra
                 clientes.add(nuevoCliente);					//Lo agrega al final
                 return true;
             }
         }
     }
 	
-    public boolean eliminarCliente (String rutCliente) {		//Metodo para eliminar un cliente de la lista
-    Cliente buscado = buscarCliente(rutCliente);		//Se busca el cliente   
-        if(buscado == null) {								//Si no se encuentra, retorna false
+    public boolean eliminarCliente (String rutCliente) {                        //Metodo para eliminar un cliente de la lista
+    Cliente buscado = buscarCliente(rutCliente);                                //Se busca el cliente   
+        if(buscado == null) {							//Si no se encuentra, retorna false
             return false;
         }
         else {
-            if(clientes.get(tamano()-1).equals(buscado)){	//En caso de encontrarlo lo elimina y retorna true
+            if(clientes.get(tamano()-1).equals(buscado)){                       //En caso de encontrarlo lo elimina y retorna true
                 clientes.remove(buscado);
                 return true;
             }
-            else {
+            else {                                                              //Sino, se actualiza el id del cliente
                 for(int i = indice(buscado)+1; i < tamano(); i++) {
                     this.clientes.get(i).setID(clientes.get(i).getID()-1);
                 }
@@ -140,7 +140,7 @@ public class ListaCliente {
         }
     }
     
-    public void modificarCliente(String rutCliente, Cliente modificado){
+    public void modificarCliente(String rutCliente, Cliente modificado){            //Metodo que modifica los datos de un cliente
         int i = indice(rutCliente);
         clientes.get(i).setNombre(modificado.getNombre());
         clientes.get(i).setApellido(modificado.getApellido());
@@ -149,8 +149,7 @@ public class ListaCliente {
         clientes.get(i).setTelefono(modificado.getTelefono());
     }
     
-    public void mostrarCliente(){
-        
+    public void mostrarCliente(){                                                   //Metodo que muestra los datos de un cliente
         for (int i = 0; i < clientes.size(); i++){
             System.out.println("Datos: "+ clientes.get(i).getNombre());
             System.out.println("Datos: "+ clientes.get(i).getApellido());
--- ProCableWW1/src/clasesproyecto/listas/ListaFactura.java
+++ ProCableWW1/src/clasesproyecto/listas/ListaFactura.java
@@ -10,11 +10,14 @@ import clasesproyecto.*;
 public class ListaFactura {
     private ArrayList<Factura> facturas;
     
+//-------------------------------------- Constructor ------------------------------------------------//    
     
     public ListaFactura(){
         this.facturas = new ArrayList<Factura>();
     }
-
+    
+//-------------------------------------- Getter y setters ------------------------------------------------//
+    
     public ArrayList<Factura> getFacturas() {
         return facturas;
     }
@@ -23,90 +26,92 @@ public class ListaFactura {
         this.facturas = facturas;
     }    
     
-    public boolean estaVacio() {
+//-------------------------------------- Metodos ------------------------------------------------//    
+    
+    public boolean estaVacio() {                                                //Reemplazo para el metodo isEmpty
         return facturas.isEmpty();
     }
     
-    public int tamano(){
+    public int tamano(){                                                        //Reemplazo para el metodo size
         return facturas.size();
     }
     
-    public int indice(int idFactura){
-        if(estaVacio()){
+    public int indice(int idFactura){                                           //Metodo para calcular el indice en base a un id
+        if(estaVacio()){                                                        //Si la lista esta vacia retorna un valor "nulo"
             return -1;
         }
-        else{
+        else{                                                                   //Sino, recorre y entrega el la posicion en donde haya coincidencia de de id
             for(int i = 0; i < tamano(); i++){
                 if(facturas.get(i).getIdFactura() == idFactura){
                     return i;
                 }
             }
-            return -1;
+            return -1;                                                          //En caso de no encontrarla, entrega un valor "nulo"
         }
     }
     
-    public Factura buscarFactura (int idFactura){
-        if(estaVacio()){
+    public Factura buscarFactura (int idFactura){                               //Metodo que busca una factura en base a un id
+        if(estaVacio()){                                                        //Si la lista esta vacia retorna nulo
             return null;
         }
-        else{
+        else{                                                                   //De otro modo, busca la factura y en caso de encontrarla la retorna
             for(int i = 0; i < tamano(); i++){
                 if(facturas.get(i).getIdFactura() == idFactura){
                     return facturas.get(i);
                 }
             }
         }
-        return null;
+        return null;                                                            //Si no la encuentra retorna null;
     }
     
-    public ArrayList<Factura> buscarFacturasCliente (String rutCliente){ //Funcionalidad propia
+    public ArrayList<Factura> buscarFacturasCliente (String rutCliente){                //Funcionalidad propia, que busca las facturas de un cliente en base a un rut
         
         ArrayList<Factura> facturasCliente = new ArrayList<Factura>();
         
-        if(estaVacio()){
+        if(estaVacio()){                                                                //Si esta vacia la lista, retorna nulo
             return null;
         }
-        else{
+        else{                                                                           //De otro modo, busca la factura en el la lista
             for(int i = 0; i < tamano(); i++){
-                if(facturas.get(i).getClienteAbonante().getRut().equals(rutCliente)){
-                    facturasCliente.add(facturas.get(i));
+                if(facturas.get(i).getClienteAbonante().getRut().equals(rutCliente)){   //Comprueba coincidencias de rut del cliente
+                    facturasCliente.add(facturas.get(i));                               //Añade la factura en el ArrayList
                 }
             }
         }
-        return facturasCliente;
+        return facturasCliente;                                                         //Retorna el ArrayList con las facturas del cliente
     }
     
-    public boolean agregarFactura (Factura nuevaFactura){
-        if(estaVacio()){
+    public boolean agregarFactura (Factura nuevaFactura){                       //Metodo para agregar una factura a la lista
+        if(estaVacio()){                                                        //Si la lista esta vacia se agrega la factura nueva y retorna true
             facturas.add(nuevaFactura);
             return true;
         }
-        else {
+        else {                                                                  //En caso contrario, se busca en la lista la factura a agregar
             if(buscarFactura(nuevaFactura.getIdFactura()) != null){
-                return false;
+                return false;                                                   //Si la encuentra retorna false, porque la factura ya esta en la lista
             }
-            else{
+            else{                                                               //Sino, se agrega la factura a la lista y retorna true
                 facturas.add(nuevaFactura);
                 return true;
             }
         }
     }
     
-    public float totalIngresosMes (){
+    public float totalIngresosMes (){                                           //Funcionalidad propia, metodo que retorna el valor de ingresos totales en un mes
         float total = 0;
-        for(int i = 0; i < tamano(); i++){
+        for(int i = 0; i < tamano(); i++){                                      //Recorre la lista y suma los pagos de las facturas
             total += facturas.get(i).getMontoPagado();
         }        
-        return total;
+        return total;                                                           //Retorna el valor de la suma de todos estos pagos
     }
     
-    public boolean eliminarFactura (int idFactura){
-        Factura buscado = buscarFactura(idFactura);        
-        if (buscado != null){
+    public boolean eliminarFactura (int idFactura){                             //Metodo para eliminar una factura en base a su id
+        Factura buscado = buscarFactura(idFactura);                             //Variable que almacena la factura a eliminar
+        if (buscado != null){                                                   //Si se encuentra, se elimina y retorna true
             facturas.remove(buscado);
             return true;
         }
-        else{
+        else{                                                                   //En caso de no encontrarla, se retorna false
             return false;
         }
     }
